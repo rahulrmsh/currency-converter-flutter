4e2d06ef334b5ae110ec35bff83ed1c930e66b87
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-09-10 14:40:47 +0530

    app update

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:currency_converter/screens/splash.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(MainApp());
+  });
+}
+
+class MainApp extends StatefulWidget {
+  @override
+  _MainAppState createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'login',
+      routes: {
+        'splash': (context) => SplashScreen(),
+      },
+    );
+  }
+}
